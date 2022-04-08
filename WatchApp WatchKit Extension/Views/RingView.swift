4e99b0c3581b7ba56dffb97aa40d59c8bfc60c61
Dr. Andreas Plagens <andreas@plagens.eu>
2022-04-08 12:44:11 +0200

    Ring completed

--- WatchApp WatchKit Extension/Views/RingView.swift
+++ WatchApp WatchKit Extension/Views/RingView.swift
@@ -9,7 +9,23 @@ import SwiftUI
 
 struct RingView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HeaderView(title: "ACTIVITY", subtitle: "RING")
+            
+            ZStack {
+                Circle()
+                    .stroke(lineWidth: 20)
+                    .fill(Color(.darkGray))
+                Circle()
+                    .trim(from: 0.5, to: 1)
+                    .stroke(Color(.red), style: StrokeStyle(lineWidth: 12, lineCap: .round, lineJoin: .round))
+                    .rotationEffect(.degrees(180))
+                    .rotation3DEffect(.degrees(180), axis: (x: 1, y: 0, z: 0))
+            }
+            .frame(width: 130, height: 130)
+            .rotationEffect(.degrees(90), anchor: .center)
+            .padding(.top, 10)
+        }
     }
 }
 
