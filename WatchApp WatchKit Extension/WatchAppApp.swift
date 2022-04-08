11bb70b0a8c0d25dcd556962f2d04af83077a5f0
Dr. Andreas Plagens <andreas@plagens.eu>
2022-04-08 09:14:44 +0200

    Initial Commit

---
+++ WatchApp WatchKit Extension/WatchAppApp.swift
@@ -0,0 +1,19 @@
+//
+//  WatchAppApp.swift
+//  WatchApp WatchKit Extension
+//
+//  Created by Dr. Andreas Plagens on 08.04.22.
+//
+
+import SwiftUI
+
+@main
+struct WatchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
